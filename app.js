afc0563497aab960c40d70be6b7cb590013afc12
Guntur <guntur@symboinsurance.com>
2020-06-23 20:19:58 +0700

    write dummy unit test

--- app.js
+++ app.js
@@ -1,41 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(cors());
+app.get("/", (req, res) => {
+  res.json({ status: "success", message: "Welcome To Testing API" });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.post("/add", (req, res) => {
+  const { num1, num2 } = req.body;
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
+  res.json({
+    status: "success",
+    result: "Welcome To Testing API",
+    result: add(num1, num2),
+  });
 });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 
 module.exports = app;
